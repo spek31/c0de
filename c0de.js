a85c27b2e0d4997aa49eecc08cdcac9a32ee40a8
spek31 <36648804+spek31@users.noreply.github.com>
2018-02-20 06:16:55 +0700

    Add files via upload

---
+++ c0de.js
@@ -0,0 +1,15 @@
+<style type='text/css'>
+/* Komentar Facebook dan Blogger */
+.komentargabungan-page,.komentargabungan-tab{display:inline-block;background:#fafafa;color:#fff;float:left;margin:0 10px 0 0;cursor:pointer;font-weight:bold;position:relative;font-size:110%;z-index:10}
+.komentargabungan-page{background:rgba(255,255,255,1);width:100%;max-width:100%;margin:0 0 20px}
+#blogger-comments-page h4{display:none}
+#blogger-comments-page p{color:#444;}
+#blogger-comments-page {padding:0px 5px;display:none;}
+.komentargabungan-tab{background:#3b5998;padding:12px;transition:all .3s;}
+.komentargabungan-tab:nth-child(2) {background:#fda753;transition:all .3s;}
+.komentargabungan-tab:hover:nth-child(2) {background:#e1954a;}
+.komentargabungan-tab-icon {height:14px;width:auto;margin:0 3px}
+.komentargabungan-tab:hover{background-color:#324c82;}
+.inactive-select-tab {background:#aaa}
+.inactive-select-tab,.komentargabungan-tab:hover {background-color:#324c82;}
+</style>
